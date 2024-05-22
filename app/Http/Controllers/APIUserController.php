07aab5078d413301547df91ba31e7527c0ba6200
Nabil Hardiansa <nhardiansa@gmail.com>
2024-05-22 12:00:53 +0800

    fix: change to only registered user can login on mobile device

--- app/Http/Controllers/APIUserController.php
+++ app/Http/Controllers/APIUserController.php
@@ -13,7 +13,7 @@ class APIUserController extends Controller
     $password = $request->input('password');
 
     $ch = curl_init();
-    $url = "https://api.diengcalderarace.com/login";
+    $url = "https://api.diengcalderarace.com/peserta";
 
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
